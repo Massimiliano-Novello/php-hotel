c970ff169e409b7f88f163584e857274d5bf490c
Massimiliano-Novello <massinov5@gmail.com>
2023-05-04 15:30:25 +0200

    inizio esercizio hotel

---
+++ index.php
@@ -0,0 +1,111 @@
+
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+    <?php
+    $parking = $_GET["parking"];
+    $vote = $_GET["vote"]; 
+    ?>
+
+    <table class="table">
+    <thead>
+        <tr>
+            <?php foreach ($hotels as $item) { ?>
+            <th scope="col"><?php echo $item["name"] ?></th>
+            <?php } ?>   
+        </tr>
+  </thead>
+  <tbody>
+    <tr>
+        <?php foreach ($hotels as $item) { ?>
+      <td><?php echo $item["description"] ?></td>
+      <?php } ?>
+    </tr>
+    <tr>
+        <?php foreach ($hotels as $item) { ?>
+      <td><?php echo $item["vote"] ?></td>
+      <?php } ?>
+    </tr>
+    <tr>
+        <?php foreach ($hotels as $item) { ?>
+      <td><?php echo $item["parking"] ?></td>
+      <?php } ?>
+    <tr>
+    <?php foreach ($hotels as $item) { ?>
+      <td><?php echo $item["distance_to_center"] ?></td>
+      <?php } ?>
+    </tr>
+  </tbody>
+</table>
+
+
+        <form action="index.php" method="GET">
+            <div>
+                <label for="parking">Parcheggio</label>
+                <select name="parking" id="parking">
+                    <option value="tutti">Tutti gli hotel</option>
+                    <option value="parcheggio">Con parcheggio</option>
+                    <option value="senza">Senza parcheggio</option>
+                </select>
+                <label for="vote">Stelle hotel</label>
+                <input type="number" id="vote" name="vote">
+            </div>
+            <button type="submit">Cerca</button>
+            <button type="reset">Annulla</button>
+        </form>
+    </div>
+
+</body>
+</html>
